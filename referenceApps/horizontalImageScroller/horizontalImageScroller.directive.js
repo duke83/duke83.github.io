fc29429c0fbe278c6f1193ea16c94bb859924346
Kent Merrell <kmerrell@gmail.com>
2015-05-05 18:03:06 -0400

    (almost) everything works except animation

--- referenceApps/horizontalImageScroller/horizontalImageScroller.directive.js
+++ referenceApps/horizontalImageScroller/horizontalImageScroller.directive.js
@@ -9,39 +9,43 @@
 
                 link: function (scp, el, attr) {
 
-                    // viewableItemsLength is used to configure the maximum number of items this directive will show at one time.
-                    var viewableItemsLength = Number(attr.viewableItemsLength);
+                    // visableItemsLength is used to configure the maximum number of items this directive will show at one time.
+                    var visableItemsLength = Number(attr.viewableItemsLength);
 
                     //The items is a JSON array that exists in the window scope.  It is written in a razor file that is fed from sitecore
                     scp.allItems = window[attr.itemsObjectName];
 
-                    scp.imageHeight=parseInt(attr.imageHeight) + 'px'
+                    scp.imageHeight = parseInt(attr.imageHeight) + 'px'
 
 
-                    scp.visibleItems=[];
-                    for(var i=0;i<4;i++){
+                    scp.visibleItems = [];
+                    for (var i = 0; i < visableItemsLength; i++) {
                         scp.visibleItems.push(scp.allItems[i]);
                     }
 
 
-
                     scp.itemIsInWindow = function (idx) {
-                        if (idx < scp.firstItemIndex.value + viewableItemsLength && !(idx < scp.currentItem.value)) {
+                        if (idx < scp.firstItemIndex.value + visableItemsLength && !(idx < scp.currentItem.value)) {
                             return true;
                         }
                         return false;
                     };
 
-                    scp.firstItemIndex= { value: 0 };
+                    scp.firstItemIndex = {value: 0};
 
                     scp.advanceLeft = function () {
-                        if(scp.firstItemIndex.value>0)
-                        scp.firstItemIndex.value = scp.firstItemIndex.value - 1;
+                        if (scp.firstItemIndex.value > 0){
+                            scp.firstItemIndex.value = scp.firstItemIndex.value - 1;
+                            scp.visibleItems.splice(visableItemsLength-1,1);
+                            scp.visibleItems.unshift(scp.allItems[scp.firstItemIndex.value])
+                        }
                     };
 
                     scp.advanceRight = function () {
-                        if(scp.firstItemIndex.value<(scp.items.length-viewableItemsLength))
-                        scp.firstItemIndex.value = scp.firstItemIndex.value + 1;
+                        if (scp.firstItemIndex.value < (scp.allItems.length - visableItemsLength))
+                            scp.firstItemIndex.value = scp.firstItemIndex.value + 1;
+                            scp.visibleItems.splice(0,1)
+                            scp.visibleItems.push(scp.allItems[scp.firstItemIndex.value + visableItemsLength-1])
                     }
                 }
 
