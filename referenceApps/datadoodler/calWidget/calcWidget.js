22ad86d08b6be6bcd43905852994024bc7db9510
Kent Merrell <kmerrell@gmail.com>
2015-07-16 19:54:56 -0400

    Working on format of calcWidget

---
+++ referenceApps/datadoodler/calWidget/calcWidget.js
@@ -0,0 +1,60 @@
+'use strict';
+angular.module('doodlecalc',[]);
+
+angular.module('doodlecalc').directive('calcwidget',function(){
+    return{
+        restrict:'E',
+        templateUrl:'calcwidget.html',
+
+        link:function(scp,el,atr){
+
+            //this is not part of the real widget directive
+        scp.widgetitem={
+            "size"
+                :
+            {
+                "x"
+                    :
+                    3, "y"
+                :
+                3
+            }
+            ,
+            "type"
+                :
+                "calc", "widgetTitle"
+                :
+                "test", "widgetId"
+                :
+                "c1437061040036kdm@adaptive.codes", "onDashboard"
+                :
+                true, "payload"
+                :
+            {
+                "VarName"
+                    :
+                    "Studio(s)", "ShortDescription"
+                :
+                "Production studio.", "LongDescription"
+                :
+                "Name of the movie studio that produced movie.", "isSelected"
+                :
+                true, "widgetId"
+                :
+                "c1437061040036kdm@adaptive.codes"
+            }
+            ,
+            "position"
+                :
+            {
+                "0"
+                    :
+                    0, "1"
+                :
+                2
+            }
+        }
+    }
+
+    };
+});
