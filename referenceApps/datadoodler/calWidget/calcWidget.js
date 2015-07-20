a1c445a788f3ab27c6eed4782714d2271c13550a
Kent Merrell <kmerrell@gmail.com>
2015-07-19 21:01:58 -0400

    CalcWidget bind to variable rows.  Just starting to flesh it out.

--- referenceApps/datadoodler/calWidget/calcWidget.js
+++ referenceApps/datadoodler/calWidget/calcWidget.js
@@ -1,60 +1,56 @@
 'use strict';
-angular.module('doodlecalc',[]);
+angular.module('doodlecalc', []);
 
-angular.module('doodlecalc').directive('calcwidget',function(){
-    return{
-        restrict:'E',
-        templateUrl:'calcwidget.html',
+angular.module('doodlecalc').directive('calcwidget', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'calcwidget.html',
 
-        link:function(scp,el,atr){
+        link: function (scp, el, atr) {
+            scp.widgetitem = {
+                'name': 'kent', 'size': {
+                    'x': 3, 'y': 3
+                },
+                'type': 'calc',
+                'widgetTitle': 'testxxx',
+                'widgetId': 'c1437061040036kdm@adaptive.codes',
+                'onDashboard': true,
+                'payload': {
+                    'VarName': 'Studio(s)',
+                    'ShortDescription': 'Production studio.',
+                    'LongDescription': 'Name of the movie studio that produced movie.',
+                    'isSelected': true,
+                    'widgetId': 'c1437061040036kdm@adaptive.codes'
+                },
+                'position': {'0': 0, '1': 2}
+            };
+
 
             //this is not part of the real widget directive
-        scp.widgetitem={
-            "size"
-                :
-            {
-                "x"
-                    :
-                    3, "y"
-                :
-                3
-            }
-            ,
-            "type"
-                :
-                "calc", "widgetTitle"
-                :
-                "test", "widgetId"
-                :
-                "c1437061040036kdm@adaptive.codes", "onDashboard"
-                :
-                true, "payload"
-                :
-            {
-                "VarName"
-                    :
-                    "Studio(s)", "ShortDescription"
-                :
-                "Production studio.", "LongDescription"
-                :
-                "Name of the movie studio that produced movie.", "isSelected"
-                :
-                true, "widgetId"
-                :
-                "c1437061040036kdm@adaptive.codes"
-            }
-            ,
-            "position"
-                :
-            {
-                "0"
-                    :
-                    0, "1"
-                :
-                2
-            }
+            //scp.widgetitem={
+            //    'size':{
+            //        'x'
+            //            :
+            //            3, 'y'
+            //        :
+            //        3
+            //    },
+            //    'type':'calc',
+            //    'widgetTitle':'testxxx',
+            //    'widgetId':'c1437061040036kdm@adaptive.codes',
+            //    'onDashboard':true,
+            //    'payload':{
+            //        'VarName':'Studio(s)',
+            //        'ShortDescription':'Production studio.',
+            //        'LongDescription':'Name of the movie studio that produced movie.',
+            //        'isSelected':true,
+            //        'widgetId':'c1437061040036kdm@adaptive.codes'
+            //    },
+            //    'position':{'0':0, '1': 2}
+            //}
         }
-    }
 
-    };
-});
+    }
+        ;
+})
+;
