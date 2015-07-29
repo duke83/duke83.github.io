8f02de15cf44fe7264e123ea6afd719157f4a744
duke83 <kmerrell@gmail.com>
2015-07-29 19:07:50 -0400

    First pass at blooming-menu.  Basic layout css.

---
+++ referenceApps/bloomingMenu/app/bloomingMenu.directive.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular.module('app').directive('bloomingMenu', function(){
+        return{
+            restrict:'E',
+            templateUrl:"bloomingMenu.directive.html"
+        }
+    });
+
+
+})();
